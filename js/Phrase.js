0e8c57ed9c926b1d8fdb3de87d36d98523a29aad
Chagorovski <52652042+Chagorovski@users.noreply.github.com>
2019-10-10 16:40:47 +0200

    bug fixes

--- js/Phrase.js
+++ js/Phrase.js
@@ -1,39 +1,34 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * Phrase.js */
-
 class Phrase {
-    constructor (phrase) {
+    constructor(phrase){
         this.phrase = phrase.toLowerCase();
-    }
-
+    };
+    
 /**
 * Display phrase on game board
 */
     addPhraseToDisplay() {
         const phraseLetters = this.phrase.split('');
         phraseLetters.map(letter => {
-                const div = document.getElementById('phrase');
-                const ul = div.firstElementChild;
-                const li = document.createElement('li');
-                
-                if (letter !== ' '){
-                    li.className = `hide letter ${letter}`;
-                }
-                else {
-                    li.className = "hide space";
-                }
-                li.textContent = letter.toUpperCase();
-                ul.appendChild(li);
-        });
-    }; 
-    
+            const div = document.getElementById('phrase');
+            const ul = div.firstElementChild;
+            const li = document.createElement('li');
+            
+            if (letter !== ' ') {
+                li.className = `hide letter ${letter}`;
+            } else {
+                li.className = "hide space";
+            }
+            li.textContent = letter.toUpperCase();
+            ul.appendChild(li);
+        });    
+    };
+
 /**
 * Checks if passed letter is in phrase
 * @param (string) letter - Letter to check
 */
-    checkLetter(letter) {
-        if (this.phrase.includes(letter)) {
+    checkLetter(selectedLetter) {
+        if (this.phrase.includes(selectedLetter)) {
             return true;
         } else {
             return false;
@@ -44,7 +39,7 @@ class Phrase {
 * Displays passed letter on screen after a match is found
 * @param (string) letter - Letter to display
 */
-    showMatchedLetter(letter) {
-        letter.className += ' show';
-    };    
-}
+    showMatchedLetter(li) {
+        li.className += ' show';
+    };
+};
