f4ef8da9898ba1240575337df5de7638a2256b68
Chagorovski <52652042+Chagorovski@users.noreply.github.com>
2019-10-10 09:40:34 +0200

    event on game button

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+
+ let game;
+ const startGameButton = document.getElementById('btn__reset');
+
+ // Listens for the start button  
+ startGameButton.addEventListener('click',function () {
+     game = new Game;
+     game.startGame();
+ })
